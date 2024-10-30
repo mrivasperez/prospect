ed0327bb57766060c72d70813d78a4ccb522ee71
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-10-29 17:16:13 -0700

    ui: prototype navigation bar

--- src/renderer/src/components/AddressBar.tsx
+++ src/renderer/src/components/AddressBar.tsx
@@ -6,17 +6,19 @@ const AddressBar = ({
   setUrl: React.Dispatch<React.SetStateAction<string>>
 }): JSX.Element => {
   return (
-    <nav className="navbar bg-primary text-white p-2 border-bottom-dark sticky-top" id="drag">
-      <div className="container">
-        <input
-          type="text"
-          className="form-control border-primary shadow-sm"
-          id="no-drag"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-      </div>
-    </nav>
+    <div
+      className="d-flex bg-body-tertiary align-center border-bottom-dark sticky-top p-2 ps-0"
+      id="drag"
+    >
+      <p className="text-secondary m-0 p-0 align-center mx-2 p-2">Address</p>
+      <input
+        type="text"
+        className="form-control"
+        id="no-drag"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+    </div>
   )
 }
 
--- src/renderer/src/feature/navigation/NavigationBar.tsx
+++ src/renderer/src/feature/navigation/NavigationBar.tsx
@@ -1,13 +1,18 @@
+import goBack from './assets/go-previous.svg'
+import goForward from './assets/go-next.svg'
 
 const NavigationBar = (): JSX.Element => {
   return (
-    <div className="d-flex justify-content-between">
-      <div>
-        <button>Back</button>
-        <button>Forward</button>
+    <div className="d-flex justify-content-between p-2 border-bottom bg-body-tertiary" id="drag">
+      <div className="d-flex">
+        <button className="btn  d-flex align-center" id="no-drag">
+          <img src={goBack} height={40} />
+          <p className="m-0 py-2 ms-0 mt-0 fw-medium">Back</p>
+        </button>
+        <button className="btn p-0" id="no-drag">
+          <img src={goForward} className="lead" height={40} />
+        </button>
       </div>
-
-      <button>X</button>
     </div>
   )
 }
