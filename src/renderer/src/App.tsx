8297bdb378646efa481e2371f2536b0511cc7a15
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-11-01 11:36:56 -0700

    feat: url updates when user navigates

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './assets/styles.css'
 import AddressBar from './components/AddressBar'
 import useWindowsDimensions from './hooks/useWindowsDimensions'
@@ -10,6 +10,23 @@ function App(): JSX.Element {
   const [url, setUrl] = useState<string>('')
   const windowDimensions = useWindowsDimensions()
   const webviewRef = useRef<HTMLWebViewElement>(null)
+
+  useEffect(() => {
+    if (webviewRef.current) {
+      webviewRef.current.addEventListener('did-navigate', (event) => {
+        console.log(event)
+        // TODO resolve
+        // @ts-expect-error url is any type
+        setUrl(event.url)
+      })
+    }
+
+    return () => {
+      if (webviewRef.current) {
+        webviewRef.current.removeEventListener('did-navigate', () => {})
+      }
+    }
+  }, [])
 
   return (
     <>
